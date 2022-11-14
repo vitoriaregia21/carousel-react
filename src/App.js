bd401d4fcbbe76ab68d585d9ea30ca71eaac9842
vitoriaregia21 <vitoriasilva8316@gmail.com[200~git config --global user.email vitoriasilva8316@gmail.com>
2022-11-13 21:00:39 -0300

    Criando carousel com React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { motion } from 'framer-motion';
+import { useState, useEffect, useRef } from 'react';
+
+import image1 from './images/1.jpg'
+import image2 from './images/2.jpg'
+import image3 from './images/3.jpg'
+import image4 from './images/4.jpg'
+import image5 from './images/5.jpg'
+import image6 from './images/6.jpg'
+import image7 from './images/7.jpg'
+import image8 from './images/8.jpg'
+import image9 from './images/9.jpg'
+import image10 from './images/10.jpg'
+
+const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10]
+
+
+
 
 function App() {
+  const carousel = useRef();
+  const [width, setWidth] = useState(0)
+
+  useEffect(() => {
+    console.log(carousel.current?.scrollWidth, carousel.current?.offseWidth);
+    setWidth(carousel.current?.scrollWidth - carousel.current?.offseWidth)
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <motion.div ref={carousel} className='carousel' whileTap={{ cursor: "grabbing" }}>
+
+        <motion.div
+          className='interna'
+          drag="x"
+          dragConstraints={{ right: 0, left: - 2000 }}
+          initial={{ x: 100 }}
+          animate ={{x:0}}
+          transition ={{duration: 0.8}}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+        {images.map(images => (
+          <motion.div className="item" key={images}>
+            <img src={images} alt="Texto alt" />
+
+          </motion.div>
+        ))}
+      </motion.div>
+    </motion.div>
+
+    </div >);
+
 }
 
 export default App;
